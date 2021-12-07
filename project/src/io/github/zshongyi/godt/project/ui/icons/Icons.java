a75a92a12581ea4cd3b5d9c7e9652fb367ca4fff
zshongyi <krwy0330@163.com>
2021-12-07 22:52:18 +0800

    Fix resources path issues

--- project/src/io/github/zshongyi/godt/project/ui/icons/Icons.java
+++ project/src/io/github/zshongyi/godt/project/ui/icons/Icons.java
@@ -8,6 +8,6 @@ package io.github.zshongyi.godt.project.ui.icons;
  *
  */
 public class Icons {
-	public static final String GO_PROJECT_LARGE = "go-project_large.png";
-	public static final String GO_PROJECT_SMALL = "go-project_small.png";
+	public static final String GO_PROJECT_LARGE = "/icons/go-project_large.png";
+	public static final String GO_PROJECT_SMALL = "/icons/go-project_small.png";
 }
